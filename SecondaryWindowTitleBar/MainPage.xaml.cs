95a3114131f23598343531a8bc33c13390383c91
prithvi.v <prithvi.v@zohocorp.com>
2021-06-14 17:42:28 +0530

    Add project files.

---
+++ SecondaryWindowTitleBar/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.WindowManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SecondaryWindowTitleBar
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public static Dictionary<string, AppWindow> AppWindows { get; set; }
+        = new Dictionary<string, AppWindow>();
+
+        // ...
+
+        private async void Grid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            // Create a new window.
+            AppWindow appWindow = await AppWindow.TryCreateAsync();
+
+            // Create a Frame and navigate to the Page you want to show in the new window.
+            // Add the new page to the Dictionary using the UIContext as the Key.
+            Frame appWindowContentFrame = new Frame();
+            AppWindows.Add("100", appWindow);
+            appWindow.Title = "App Window " + AppWindows.Count.ToString();
+            appWindowContentFrame.Navigate(typeof(MeetingPage));
+
+            // Attach the XAML content to the window.
+            ElementCompositionPreview.SetAppWindowContent(appWindow, appWindowContentFrame);
+
+
+
+            // When the window is closed, be sure to release
+            // XAML resources and the reference to the window.
+            appWindow.Closed += delegate
+            {
+                MainPage.AppWindows.Remove("100");
+                appWindowContentFrame.Content = null;
+                appWindow = null;
+            };
+
+            // Show the window.
+            await appWindow.TryShowAsync();
+        }
+
+    }
+}
---
+++ SecondaryWindowTitleBar/MeetingPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.WindowManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SecondaryWindowTitleBar
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MeetingPage : Page
+    {
+        AppWindow window;
+        public MeetingPage()
+        {
+            this.InitializeComponent();
+            Loaded += MeetingPage_Loaded;
+            //var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            //coreTitleBar.ExtendViewIntoTitleBar = true;
+            //coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+            window = MainPage.AppWindows["100"];
+            window.TitleBar.ExtendsContentIntoTitleBar = true;
+            Window.Current.SetTitleBar(AppTitleBar);
+        }
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            AppTitleBar.Height = sender.Height;
+        }
+
+        private void MeetingPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Get the reference to this AppWindow that was stored when it was created.
+            window = MainPage.AppWindows["100"];
+
+            // Set up event handlers for the window.
+            window.Changed += Window_Changed;
+        }
+
+        private void Window_Changed(AppWindow sender, AppWindowChangedEventArgs args)
+        {
+        }
+    }
+}
